480ed9a5fc273dfbb736b0c891efe7b7be682431
sshan <shhan@covision.co.kr>
2024-11-12 17:20:27 +0900

    iso 인증 화면 추가

---
+++ src/main/java/com/dongwoo/SQM/auditMgmt/dto/IsoSearchResult.java
@@ -0,0 +1,24 @@
+package com.dongwoo.SQM.auditMgmt.dto;
+
+import com.dongwoo.SQM.board.dto.PageDTO;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+public class IsoSearchResult {
+    private List<IsoAuthDTO> resultList;  // 검색된 항목들
+    private PageDTO pageMaker;           // 페이지 네비게이션 정보
+    // 기본 생성자
+    public IsoSearchResult() {}
+
+    // 검색된 항목들과 페이지네이션 정보를 받는 생성자
+    public IsoSearchResult(List<IsoAuthDTO> items, PageDTO pageMaker) {
+        this.resultList = items;
+        this.pageMaker = pageMaker;
+    }
+}
