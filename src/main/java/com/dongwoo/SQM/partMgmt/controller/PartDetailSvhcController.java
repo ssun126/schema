061b4a84df7c5d86f3bd223063add11419cd0138
hansolkim <gt1874@naver.com>
2024-11-29 10:21:42 +0900

    DB정보 변경 적용

--- src/main/java/com/dongwoo/SQM/partMgmt/controller/PartDetailSvhcController.java
+++ src/main/java/com/dongwoo/SQM/partMgmt/controller/PartDetailSvhcController.java
@@ -5,6 +5,7 @@ import com.dongwoo.SQM.partMgmt.service.PartDetailService;
 import com.dongwoo.SQM.siteMgr.dto.DeclarationDTO;
 import com.dongwoo.SQM.siteMgr.dto.SvhcListDTO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.HashMap;
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/user/partMgmt")
@@ -20,9 +22,11 @@ public class PartDetailSvhcController {
 
     private final PartDetailService partDetailService;
 
-    @GetMapping("/getSvhcData")
+    @GetMapping("/getSvhcListData")
     public List<SvhcListDTO> getSvhcData(){
+        log.info("test=-11111=================");
         List<SvhcListDTO> svhcList = partDetailService.getSvhcData();
+        log.info("test=-11111================="+svhcList);
 
         return svhcList;
     }
@@ -30,7 +34,7 @@ public class PartDetailSvhcController {
     @GetMapping("/getDeclarData")
     public List<DeclarationDTO> getDeclarData(){
         List<DeclarationDTO> declarList = partDetailService.getDeclarData();
-
+        log.info("test=3232222-================="+declarList);
         return declarList;
     }
 
