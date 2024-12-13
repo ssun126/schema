df0e5942fc7845ae184cad9b63e14b617c753600
sshan <shhan@covision.co.kr>
2024-12-13 18:14:54 +0900

    품질관리 첨부파일 업로드/제출

--- src/main/java/com/dongwoo/SQM/auditMgmt/controller/QualityControlController.java
+++ src/main/java/com/dongwoo/SQM/auditMgmt/controller/QualityControlController.java
@@ -1,15 +1,25 @@
 package com.dongwoo.SQM.auditMgmt.controller;
 
+import com.dongwoo.SQM.auditMgmt.dto.AuditMgmtDTO;
+import com.dongwoo.SQM.auditMgmt.service.AuditCommonService;
+import com.dongwoo.SQM.config.security.UserCustom;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class QualityControlController {
+    @Autowired
+    private AuditCommonService auditCommonService;
+
     @GetMapping("/admin/auditMgmt/qualityControl")
     public String GetList(Model model) {
         return "qualityControl/adminMain";
@@ -23,7 +33,11 @@ public class QualityControlController {
     @GetMapping("/user/auditMgmt/qualityControl")
     public String GetUserList(Model model) {
         //업체의 품질관리 Audit 기본 정보를 보여준다.
+        UserCustom user = (UserCustom) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String comCode = user.getCOM_CODE();
 
+        List<AuditMgmtDTO> companyAuthFile = auditCommonService.getCompanyAuthFile("QUALITY", comCode);
+        model.addAttribute("companyAuthFile", companyAuthFile);
 
         return "qualityControl/main";
     }
