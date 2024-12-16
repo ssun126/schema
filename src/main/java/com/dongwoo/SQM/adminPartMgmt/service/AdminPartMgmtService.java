7f1620a626c45fbe3f66c467d3b20025fec37b5e
hansolkim <gt1874@naver.com>
2024-12-16 20:46:05 +0900

    자재만료일관리(svhc미완)

--- src/main/java/com/dongwoo/SQM/adminPartMgmt/service/AdminPartMgmtService.java
+++ src/main/java/com/dongwoo/SQM/adminPartMgmt/service/AdminPartMgmtService.java
@@ -2,7 +2,6 @@ package com.dongwoo.SQM.adminPartMgmt.service;
 
 import com.dongwoo.SQM.adminPartMgmt.dto.AdminPartMgmtDTO;
 import com.dongwoo.SQM.adminPartMgmt.repository.AdminPartMgmtRepository;
-import com.dongwoo.SQM.partMgmt.dto.PartMgmtDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -25,5 +24,13 @@ public class AdminPartMgmtService {
 
     public List<HashMap> getPartMSDSExpList(String EXP_DATE, String COM_CODE, String COM_NAME) {
         return adminPartMgmtRepository.getPartMSDSExpList(EXP_DATE, COM_CODE, COM_NAME);
+    }
+
+    public List<HashMap> getPartDeclExpList(String EXP_DATE, String COM_CODE, String COM_NAME) {
+        return adminPartMgmtRepository.getPartDeclExpList(EXP_DATE, COM_CODE, COM_NAME);
+    }
+
+    public List<HashMap> getPartEtcExpList(String EXP_DATE, String COM_CODE, String COM_NAME) {
+        return adminPartMgmtRepository.getPartDeclExpList(EXP_DATE, COM_CODE, COM_NAME);
     }
 }
