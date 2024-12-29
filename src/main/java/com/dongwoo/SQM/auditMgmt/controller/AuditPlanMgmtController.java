f2a5abb6b54bc534ea3bb760a2078eb7f2bcf7b2
sshan <shhan@covision.co.kr>
2024-12-29 22:04:14 +0900

    Audit 계획 API 연동

--- src/main/java/com/dongwoo/SQM/auditMgmt/controller/AuditPlanMgmtController.java
+++ src/main/java/com/dongwoo/SQM/auditMgmt/controller/AuditPlanMgmtController.java
@@ -22,6 +22,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -51,48 +53,54 @@ public class AuditPlanMgmtController {
      */
 
 	@PostMapping("/api/audit/auditPlanMgmt/getCodeList")
-	public ResponseEntity<Map<String, Object>> getCodeList(
-			@RequestBody String param,
-			HttpServletRequest request,
-			HttpServletResponse response
-			) throws Exception {
-		Map<String, Object> responseData = new HashMap<>();
-    	// JSON Parameter Parsing
-    	Map<String, Object> reqParam = reqToMap(request);
-    	MultiValueMap<String, Object> bodyMap = new LinkedMultiValueMap<String, Object>();
-    	HttpHeaders headers = new HttpHeaders();
-    	ArrayList arrayList = new ArrayList();
-    	try {
-    		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        	HttpEntity<MultiValueMap<String,Object>> requestEntity = new HttpEntity<>(bodyMap, headers);
-        	Map<String, String> tokenInfo = (Map<String, String>) request.getAttribute("TOKEN_INFO");
+	public ResponseEntity<Map<String, Object>> getCodeList(@RequestParam(value = "param", required = false) String param, HttpServletRequest request) throws Exception {
+		Map<String, Object> response = new HashMap<>();
+		Map<String, Object> reqParam = commonService.jsonDataMap(param);
+		MultiValueMap<String, Object> bodyMap = new LinkedMultiValueMap<String, Object>();
+		HttpHeaders headers = new HttpHeaders();
+		ArrayList arrayList = new ArrayList();
+		try {
+			headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+			HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(bodyMap, headers);
+			bodyMap.add("fnc", "GetCodeList");
+			bodyMap.add("pFlag", "PARTNER_AUDIT_TYPE");
+			bodyMap.add("pGroupCd", "9999");
+			bodyMap.add("USERID", "covision");
+			bodyMap.add("SITEID", "DW01");
+			RestTemplate restTemplate = new RestTemplate();
+			try {
+				log.info("requestEntity=====================================>" + requestEntity);
+				ResponseEntity<String> responseEn = restTemplate.exchange(povisInterfaceURL, HttpMethod.POST, requestEntity, String.class);
+				log.info("responseEn=====================================>" + responseEn.getBody());
 
-        	bodyMap.add("fnc", "GetCodeList");
-        	bodyMap.add("pFlag", reqParam.get("pFlag").toString());
-        	bodyMap.add("pGroupCd", reqParam.get("pGroupCd").toString());
-        	RestTemplate restTemplate = new RestTemplate();
-        	ResponseEntity<String> responseEn = restTemplate.exchange(povisInterfaceURL, HttpMethod.POST, requestEntity, String.class);
+				String jsonResponse = responseEn.getBody();
+				ObjectMapper objectMapper = new ObjectMapper();
+				JsonNode rootNode = objectMapper.readTree(jsonResponse);
 
-			String jsonResponse = responseEn.getBody();
-			ObjectMapper objectMapper = new ObjectMapper();
-			JsonNode rootNode = objectMapper.readTree(jsonResponse);
-			// Extract dataList
-			JsonNode dataList = rootNode.path("dataList");
-			// Iterate over the dataList
-			if (dataList.isArray()) {
-				for (JsonNode item : dataList) {
-					arrayList.add(item);
+				// Extract dataList
+				JsonNode dataList = rootNode.path("dataList");
+				// Iterate over the dataList
+				if (dataList.isArray()) {
+					for (JsonNode item : dataList) {
+						arrayList.add(item);
+					}
 				}
+
+				response.put("dataList", arrayList);
+			} catch (HttpClientErrorException | HttpServerErrorException ex) {
+				System.out.println("HTTP Status: " + ex.getStatusCode());
+				System.out.println("Response Body: " + ex.getResponseBodyAsString());
+			} catch (Exception ex) {
+				ex.printStackTrace();  // 다른 예외를 처리
 			}
-        	
-			responseData.put("dataList", arrayList);
+
 		} catch (Exception e) {
 			log.info("Exception/Start(auditPlanMgmt/getCodeList)============================>");
-			log.info(e.getMessage());
+			log.info(bodyMap.toString());
+			log.info("", e);
 			log.info("Exception/End(auditPlanMgmt/getCodeList)==============================>");
 		}
-
-		return ResponseEntity.ok(responseData);
+		return ResponseEntity.ok(response);  // JSON 응답 반환
 	}
 	/**
      * AuditPlan LIST
